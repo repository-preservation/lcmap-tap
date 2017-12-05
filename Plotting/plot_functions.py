Fixed scaling to get correct idx calc results

Plotting/plot_functions.py
@@ -10,7 +10,7 @@ def test_for_zero(num):
     """
     # mask = np.zeros_like(num, dtype=np.bool)
 
-    num[num == 0] = 0.001
+    num[num == 0.0] = 0.001
     # mask[num != 0.0] = True
 
     return num
@@ -35,13 +35,14 @@ def msavi(R, NIR):
     :param NIR:
     :return:
     """
-    sqrt = test_for_negative((2.0 * NIR + 1.0) ** 2.0 - 8.0 * (NIR - R))
+    sqrt = (2.0 * (NIR * 0.0001) + 1.0) ** 2.0 - 8.0 * ((NIR * 0.0001) - (R * 0.0001))
+    sqrt = test_for_negative(sqrt)
 
-    result = (2.0 * NIR + 1.0 - (sqrt ** 0.5)) / 2.0
+    result = (2.0 * (NIR * 0.0001) + 1.0 - (sqrt ** 0.5)) / 2.0
 
-    result[result >= 1.0] = 1.0
+    # result[result >= 1.0] = 1.0
 
-    result[result <= -1.0] = -1.0
+    # result[result <= -1.0] = -1.0
 
     return result
 
@@ -54,22 +55,24 @@ def ndvi(R, NIR):
     :param NIR:
     :return:
     """
-    res = test_for_zero(NIR + R)
+    den = NIR + R
+
+    den = test_for_zero(den)
 
     # result = np.zeros_like(R, dtype=np.float32)
 
     # result[mask] = (NIR[mask] - R[mask]) / (NIR[mask] + R[mask])
 
-    result = (NIR - R) / res
+    result = (NIR - R) / den
 
-    result[result >= 1.0] = 1.0
+    # result[result >= 1.0] = 1.0
 
-    result[result <= -1.0] = -1.0
+    # result[result <= -1.0] = -1.0
 
     return result
 
 
-def evi(B, R, NIR, G=2.5, L=1.0, C1=6, C2=7.5):
+def evi(B, R, NIR, G=2.5, L=1.0, C1=6.0, C2=7.5):
     """
     Enhanced Vegetation Index
     G * ((NIR - R) / (NIR + C1 * R - C2 * B + L))
@@ -82,14 +85,14 @@ def evi(B, R, NIR, G=2.5, L=1.0, C1=6, C2=7.5):
     :param C2: <float> Constant
     :return:
     """
-    res = test_for_zero(NIR + C1 * R - C2 * B + L)
-    # result = np.zeros_like(R, dtype=np.float32)
+    den = (NIR * 0.0001) + C1 * (R * 0.0001) - C2 * (B * 0.0001) + L
+    den = test_for_zero(den)
 
     # result[mask] = G * ((NIR[mask] - R[mask]) / (NIR[mask] + C1 * R[mask] - C2 * B[mask] + L))
-    result = G * ((NIR - R) / res)
+    result = G * (((NIR * 0.0001) - (R * 0.0001)) / den)
 
-    result[result >= 5] = 0
-    result[result <= -5] = 0
+    # result[result >= 5] = 0
+    # result[result <= -5] = 0
 
     return result
 
@@ -103,11 +106,12 @@ def savi(R, NIR, L=0.5):
     :param L:
     :return:
     """
-    res = test_for_zero(NIR + R + L)
+    den = (NIR * 0.0001) + (R * 0.0001) + L
+    den = test_for_zero(den)
     # result = np.zeros_like(R, dtype=np.float32)
 
     # result[mask] = ((NIR[mask] - R[mask]) / (NIR[mask] +R[mask] + L)) * (1 + L)
-    result = ((NIR - R) / (res)) * (1 + L)
+    result = (((NIR * 0.0001) - (R * 0.0001)) / den) * (1 + L)
 
     return result
 
@@ -120,11 +124,12 @@ def ndmi(NIR, SWIR1):
     :param SWIR1:
     :return:
     """
-    res = test_for_zero(NIR + SWIR1)
+    den = NIR + SWIR1
+    den = test_for_zero(den)
     # result = np.zeros_like(NIR, dtype=np.float32)
 
     # result[mask] = (NIR[mask] - SWIR1[mask]) / (NIR[mask] + SWIR1[mask])
-    result = (NIR - SWIR1) / res
+    result = (NIR - SWIR1) / den
 
     return result
 
@@ -137,11 +142,12 @@ def nbr(NIR, SWIR2):
     :param SWIR2:
     :return:
     """
-    res = test_for_zero(NIR + SWIR2)
+    den = NIR + SWIR2
+    den = test_for_zero(den)
     # result = np.zeros_like(NIR, dtype=np.float32)
 
     # result[mask] = (NIR[mask] - SWIR2[mask]) / (NIR[mask] + SWIR2[mask])
-    result = (NIR - SWIR2) / res
+    result = (NIR - SWIR2) / den
 
     return result
 
@@ -154,10 +160,11 @@ def nbr2(SWIR1, SWIR2):
     :param SWIR2:
     :return:
     """
-    res = test_for_zero(SWIR1 + SWIR2)
+    den = SWIR1 + SWIR2
+    den = test_for_zero(den)
     # result = np.zeros_like(SWIR1, dtype=np.float32)
 
     # result[mask] = (SWIR1[mask] - SWIR2[mask]) / (SWIR1[mask] + SWIR2[mask])
-    result = (SWIR1 - SWIR2) / res
+    result = (SWIR1 - SWIR2) / den
 
     return result