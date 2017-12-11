f2bd1f7f27277dc7ba1e92646a6f5ea711dd36bd
danzelenak-usgs <daniel.zelenak.ctr@usgs.gov>
2017-12-11 09:04:37 -0600

    Check for python version 3

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from Controls.controls import PlotControls
 
+if sys.version_info[0] < 3:
+    raise Exception("Python 3.* is required")
+
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
