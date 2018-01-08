4ef29243e40650cc337826df963525a15094fb15
danzelenak-usgs <daniel.zelenak.ctr@usgs.gov>
2018-01-08 14:53:55 -0600

    New script to generate ARD view

---
+++ pyccd_plotter/Visualization/ard_viewer.py
@@ -0,0 +1,63 @@
+
+import datetime as dt
+
+import matplotlib
+import numpy as np
+from PyQt5 import QtWidgets
+
+matplotlib.use("Qt5Agg")
+
+from matplotlib.collections import PathCollection
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+
+class MplCanvas(FigureCanvas):
+    def __init__(self, fig):
+        self.fig = fig
+
+        FigureCanvas.__init__(self, self.fig)
+
+
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Minimum)
+
+
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+
+        FigureCanvas.setSizePolicy(self, sizePolicy)
+
+        FigureCanvas.updateGeometry(self)
+
+
+class ARDViewer(QtWidgets.QMainWindow):
+    def __init__(self, fig, parent=None):
+
+        super(ARDViewer, self).__init__(parent)
+
+        self.widget = QtWidgets.QWidget()
+        self.setCentralWidget(self.widget)
+        self.widget.setLayout(QtWidgets.QVBoxLayout())
+        self.widget.layout().setContentsMargins(0, 0, 0, 0)
+        self.widget.layout().setSpacing(0)
+
+        self.fig = fig
+        self.canvas = MplCanvas(fig=self.fig)
+        self.canvas.draw()
+
+
+        self.nav = NavigationToolbar(self.canvas, self.widget)
+
+        self.widget.layout().addWidget(self.nav)
+
+        self.widget.layout().addWidget(self.canvas)
+
+        self.scroll = QtWidgets.QScrollArea(self.widget)
+        self.scroll.setWidgetResizable(True)
+
+        self.scroll.setWidget(self.canvas)
+
+        self.widget.layout().addWidget(self.scroll)
+
+        self.show()
