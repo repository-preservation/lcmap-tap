46a034476b8b0caf75368348457984d3153ca19a
danzelenak-usgs <daniel.zelenak.ctr@usgs.gov>
2018-05-09 11:15:10 -0500

    Updated logger

--- lcmap_tap/logger.py
+++ lcmap_tap/logger.py
@@ -1,6 +1,13 @@
+"""Create a logger"""
+
+import os
 import logging
 import sys
 import time
+from pathlib import Path
+
+# Get the path to the home directory for the current user
+HOME = str(Path.home())
 
 
 def get_time():
@@ -16,12 +23,12 @@ def get_time():
 
 log = logging.getLogger()
 stream_handler = logging.StreamHandler(sys.stdout)
-file_handler = logging.FileHandler("C:/users/dzelenak/workspace/lcmap_tap_{}.log".format(get_time()))
-formatter = logging.Formatter('%(asctime)s %(processName)s: %(message)s')
+file_handler = logging.FileHandler(os.path.join(HOME, "lcmap_tap_{}.log".format(get_time())))
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(processName)s: %(message)s')
 
 stream_handler.setFormatter(formatter)
 file_handler.setFormatter(formatter)
 
 log.addHandler(stream_handler)
 log.addHandler(file_handler)
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
