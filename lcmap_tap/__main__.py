5cfdf9b7325685005c47d7a955d74104a3132293
danzelenak-usgs <daniel.zelenak.ctr@usgs.gov>
2018-05-09 09:32:19 -0500

    Added logging

--- lcmap_tap/__main__.py
+++ lcmap_tap/__main__.py
@@ -1,18 +1,26 @@
 import sys
-
+from lcmap_tap.logger import log
 from PyQt5.QtWidgets import QApplication
-
 from lcmap_tap.Controls.controls import MainControls
 
 
-def main():
-    # session_id = "session_{}".format(MainControls.get_time())
+def exc_handler(exception):
+    log.exception("Exception Occurred: {}".format(str(exception[1])))
 
+
+def main():
+    # Create a QApplication object, necessary to manage the GUI control flow and settings
     app = QApplication(sys.argv)
+
+    sys.excepthook = exc_handler
+
+    # session_id = "session_{}".format(MainControls.get_time())
 
     control_window = MainControls()
 
     if control_window:
+        # Enter the main event loop, begin event handling for application widgets until exit() is called
+
         sys.exit(app.exec_())
 
 
