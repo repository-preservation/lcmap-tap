31b13971f17ed336cbfe19b349887d43f451e28d
danzelenak-usgs <dzelenak@usgs.gov>
2018-05-09 09:25:06 -0500

    Updated requirements, version, description

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
-"""The Time Series Analysis and Plotting (TAP) tool is being developed to provide visualization and analysis support of
-LCMAP products generated with PyCCD.  Multispectral time-series models and calculated indices at a specified point
-location are available for plotting.  The plots by default include all ARD observations, PyCCD time-segment model-fits,
-time-segment attributes including start, end, and break dates, and datelines representing annual increments on day 1 of
-each year.  The tool generates an interactive Matplotlib figure that displays plots for bands and indices selected by
-the user via the GUI.
-"""
+"""This plotting tool is being developed to provide visualization and analysis support of LCMAP products generated
+with PyCCD. Multi-spectral time-series models and calculated indices at a specified point location are available for
+plotting. The plots by default include all ARD observations, PyCCD time-segment model-fits, time-segment attributes
+including start, end, and break dates, and datelines representing annual increments on day 1 of each year. The tool
+generates an interactive figure that contains the specified bands and indices, with each of these being drawn on its
+own subplot within the figure. Interactive capabilities of the figure include zooming-in to an area of interest,
+returning to the default zoom level, adjusting subplot-specific x and y axis ranges, adjusting subplot sizes,
+and saving the current figure. For each subplot the x-axis represents the dates of the time series, and the y-axis
+represents that subplot’s band values. Both axes are rescaled and relabeled on zoom events allowing for finer
+resolution at smaller scales. Each subplot has interactive picking within the plotting area and within the legend.
+Left-clicking on observation points in the subplot displays the information associated with that observation in a
+window on the GUI. Left-clicking on items in the legend turns on/off those items in the subplot. Button controls on
+the GUI allow for generating and displaying the plot, clearing the list of clicked observations, saving the figure in
+its current state to a .PNG image file, and exiting out of the tool. """
 
 from setuptools import setup, find_packages
 
 setup(
     name='lcmap_tap',
 
-    version='0.1.0',
+    version='0.1.2',
 
     packages=find_packages(),
 
@@ -24,7 +31,7 @@ setup(
 
     entry_points={'gui_scripts': ['lcmap_tap = lcmap_tap.__main__:main']},
 
-    dependency_links=['https://github.com/conda-forge/gdal-feedstock/'],
+    # dependency_links=['https://github.com/conda-forge/gdal-feedstock/'],
 
     python_requires='>=3.5',
 
