165236f6bc1a54117697594d91ff864832ef558a
danzelenak-usgs <daniel.zelenak.ctr@usgs.gov>
2018-06-15 13:11:02 -0500

    Remove GeoInfo instantiation

--- lcmap_tap/RetrieveData/retrieve_ard.py
+++ lcmap_tap/RetrieveData/retrieve_ard.py
@@ -1,8 +1,8 @@
 """Read a chip of ARD using lcmap-merlin"""
 
-from lcmap_tap.RetrieveData.retrieve_data import GeoInfo
 from lcmap_tap.RetrieveData.retrieve_data import GeoCoordinate
 from lcmap_tap.logger import log
+import sys
 import time
 import yaml
 import merlin
@@ -12,10 +12,25 @@ try:
 
     log.info("lcmap-merlin url=%s" % MERLIN)
 
-    print(MERLIN)
-
 except FileNotFoundError:
     MERLIN = None
+
+
+def exc_handler(exc_type, exc_value, exc_traceback):
+    """
+    Customized handling of top-level exceptions
+    Args:
+        exc_type: exception class
+        exc_value: exception instance
+        exc_traceback: traceback object
+
+    Returns:
+
+    """
+    log.critical("Exception: ", exc_info=(exc_type, exc_value, exc_traceback))
+
+
+sys.excepthook = exc_handler
 
 
 def get_time():
@@ -31,21 +46,21 @@ def get_time():
 class ARDData:
     """Use lcmap-merlin to retrieve a time-series ARD for a chip"""
 
-    def __init__(self, x: str, y: str, start: str='1982-01-01', stop: str='2017-12-31'):
+    def __init__(self, coord: GeoCoordinate, pixel_coord: GeoCoordinate,
+                 start: str='1982-01-01', stop: str='2017-12-31'):
         """
 
         Args:
-            x: X-coordinate in projected meters
-            y: Y-coordinate in projected meters
+            coord: The X and Y coordinates of the target point in projected meters
+            pixel_coord: The upper left coordinate of the pixel in projected meters
             start: The start date (YYYY-MM-DD) of the time series
             stop: The stop date (YYYY-MM-DD) of the time series
 
         """
-        self.geo_info = GeoInfo(x, y)
-
         t0 = get_time()
-        self.timeseries = merlin.create(x=self.geo_info.geo_coord.x,
-                                        y=self.geo_info.geo_coord.y,
+
+        self.timeseries = merlin.create(x=coord.x,
+                                        y=coord.y,
                                         acquired="{}/{}".format(start, stop),
                                         cfg=merlin.cfg.get(profile="chipmunk-ard",
                                                            env={"CHIPMUNK_URL": MERLIN}))
@@ -54,7 +69,7 @@ class ARDData:
         log.info("Time series retrieved in %s seconds" % (t1 - t0))
 
         self.pixel_ard = self.get_sequence(timeseries=self.timeseries,
-                                           pixel_coord=self.geo_info.pixel_coord)
+                                           pixel_coord=pixel_coord)
 
     @staticmethod
     def get_sequence(timeseries: tuple, pixel_coord: GeoCoordinate) -> dict:
@@ -62,11 +77,11 @@ class ARDData:
         Find the matching time series rod from the chip of results using pixel upper left coordinate
 
         Args:
-            timeseries: series of tuples, index 0 holds tuple of coordinates, index 1 holds dict of ARD data and dates
-            pixel_coord: has attributes x and y
+            timeseries: Series of tuples, [0] = tuple of coordinates, [1] = dict of ARD data and dates
+            pixel_coord: The upper left coordinate of the pixel in projected meters
 
         Returns:
-            Dict, keys are individual bands and dates for the time series
+            Dict, keys are band designations and dates for the time series
 
         """
         gen = filter(lambda x: x[0][2] == pixel_coord.x and x[0][3] == pixel_coord.y, timeseries)
