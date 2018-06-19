0f08bee792f9b208ea62299a268a1506df53ac7f
danzelenak-usgs <daniel.zelenak.ctr@usgs.gov>
2018-06-19 15:24:53 -0500

    Added data serialization, image_ids function

--- lcmap_tap/RetrieveData/retrieve_ard.py
+++ lcmap_tap/RetrieveData/retrieve_ard.py
@@ -2,18 +2,17 @@
 
 from lcmap_tap.RetrieveData.retrieve_data import GeoCoordinate
 from lcmap_tap.logger import log
+import os
 import sys
 import time
 import yaml
+import pickle
+import glob
 import merlin
+from pathlib import Path
 
-try:
-    MERLIN = yaml.load(open('URL.yaml', 'r'))['merlin']
 
-    log.info("lcmap-merlin url=%s" % MERLIN)
-
-except FileNotFoundError:
-    MERLIN = None
+HOME = str(Path.home())
 
 
 def exc_handler(exc_type, exc_value, exc_traceback):
@@ -43,33 +42,69 @@ def get_time():
     return time.time()
 
 
+def get_image_ids(path: str) -> list:
+    """
+    Return a list of image IDs based on the contents of the ARD tarballs folder
+
+    Args:
+        path:
+
+    Returns:
+
+    """
+    file_list = glob.glob(path + os.sep + "*SR*")
+
+    return sorted([os.path.splitext(os.path.basename(f))[0] for f in file_list])
+
+
 class ARDData:
     """Use lcmap-merlin to retrieve a time-series ARD for a chip"""
 
-    def __init__(self, coord: GeoCoordinate, pixel_coord: GeoCoordinate,
+    def __init__(self, coord: GeoCoordinate, pixel_coord: GeoCoordinate, config: str,
+                 home: str=HOME,
                  start: str='1982-01-01', stop: str='2017-12-31'):
         """
 
         Args:
             coord: The X and Y coordinates of the target point in projected meters
             pixel_coord: The upper left coordinate of the pixel in projected meters
+            config: Absolute path to a .yaml configuration file
+            home: Absolute path to a working directory that would contain serialized ARD, default is User's home dir
             start: The start date (YYYY-MM-DD) of the time series
             stop: The stop date (YYYY-MM-DD) of the time series
 
         """
-        t0 = get_time()
+        MERLIN = yaml.load(open(config, 'r'))['merlin']
 
-        self.timeseries = merlin.create(x=coord.x,
-                                        y=coord.y,
-                                        acquired="{}/{}".format(start, stop),
-                                        cfg=merlin.cfg.get(profile="chipmunk-ard",
-                                                           env={"CHIPMUNK_URL": MERLIN}))
-        t1 = get_time()
+        p_file = os.path.join(home, "ard_{x}_{y}.p".format(x=pixel_coord.x,
+                                                           y=pixel_coord.y))
 
-        log.info("Time series retrieved in %s seconds" % (t1 - t0))
+        if not os.path.exists(p_file):
 
-        self.pixel_ard = self.get_sequence(timeseries=self.timeseries,
-                                           pixel_coord=pixel_coord)
+            t0 = get_time()
+
+            self.timeseries = merlin.create(x=coord.x,
+                                            y=coord.y,
+                                            acquired="{}/{}".format(start, stop),
+                                            cfg=merlin.cfg.get(profile="chipmunk-ard",
+                                                               env={"CHIPMUNK_URL": MERLIN}))
+            t1 = get_time()
+
+            log.info("Time series retrieved in %s seconds" % (t1 - t0))
+
+            self.pixel_ard = self.get_sequence(timeseries=self.timeseries,
+                                               pixel_coord=pixel_coord)
+
+            with open(p_file, "wb") as f:
+                pickle.dump(self.pixel_ard, f)
+
+                log.info("Dumped ARD data to pickle file %s" % p_file)
+
+        else:
+            with open(p_file, "rb") as f:
+                self.pixel_ard = pickle.load(f)
+
+            log.info("Read ARD data from pre-existing pickle file %s" % p_file)
 
     @staticmethod
     def get_sequence(timeseries: tuple, pixel_coord: GeoCoordinate) -> dict:
@@ -81,7 +116,7 @@ class ARDData:
             pixel_coord: The upper left coordinate of the pixel in projected meters
 
         Returns:
-            Dict, keys are band designations and dates for the time series
+            Dict whose keys are band designations and dates for the time series
 
         """
         gen = filter(lambda x: x[0][2] == pixel_coord.x and x[0][3] == pixel_coord.y, timeseries)
