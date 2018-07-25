b6f063eb7c1b210305f65141e504736744c6a4e6
danzelenak-usgs <dzelenak@contractor.usgs.gov>
2018-07-25 08:56:05 -0500

    Moved COLORS and NAMES to __init__

---
+++ lcmap_tap/Plotting/__init__.py
@@ -0,0 +1,20 @@
+
+COLORS = {'Developed': (0.933, 0.0, 0.0),
+          'Agriculture': (0.671, 0.439, 0.157),
+          'Grass/Shrub': (0.890, 0.890, 0.761),
+          'Tree Cover': (0.110, 0.388, 0.188),
+          'Water': (0.278, 0.420, 0.631),
+          'Wetlands': (0.729, 0.851, 0.922),
+          'Ice/Snow': (1.0, 1.0, 1.0),
+          'Barren': (0.702, 0.682, 0.639),
+          'No Model-fit': (0.984, 0.604, 0.6)}
+
+NAMES = {1: 'Developed',
+         2: 'Agriculture',
+         3: 'Grass/Shrub',
+         4: 'Tree Cover',
+         5: 'Water',
+         6: 'Wetlands',
+         7: 'Ice/Snow',
+         8: 'Barren',
+         9: 'No Model-fit'}
