0ca7bea30479bb87e4b48af2ea45b08270603400
danzelenak-usgs <dzelenak@contractor.usgs.gov>
2018-09-13 08:58:40 -0500

    Added several new functions

--- lcmap_tap/Analysis/__init__.py
+++ lcmap_tap/Analysis/__init__.py
@@ -6,3 +6,5 @@ DOY = {'summer': ['some-range'],
        'spring': ['some-range'],
        'winter': ['some-range'],
        'fall': ['some-range']}
+
+mask_values = (1, 96, 112, 160, 176, 224, 352, 368, 416, 432, 480, 864, 880, 928, 944, 992)
--- lcmap_tap/Analysis/data_tools.py
+++ lcmap_tap/Analysis/data_tools.py
@@ -1,5 +1,14 @@
 """Some helpful functions for working with a pandas DataFrame object"""
 
+from lcmap_tap.Analysis import mask_values
+import warnings
+import datetime as dt
+import pandas as pd
+import numpy as np
+from collections import OrderedDict
+
+warnings.simplefilter('ignore')
+
 
 def temporal(df, ascending=True, field='dates'):
     """
@@ -45,22 +54,159 @@ def dates(df, params, field='dates'):
         pd.DataFrame
 
     """
-    min, max = params
+    _min, _max = params
 
-    return df[(df[field] >= min) & (df[field] <= max)].reset_index(drop=True)
+    return df[(df[field] >= _min) & (df[field] <= _max)].reset_index(drop=True)
 
 
-def filter(df, value, field='qa'):
+def years(df):
+    """
+    Get an array of unique years in the current time series
+
+    Args:
+        df (pd.DataFrame): The input data frame
+
+    Returns:
+        np.ndarray
+
+    """
+    return df['dates'].apply(lambda x: (x.timetuple()).tm_year).unique()
+
+
+def date_range(params):
+    """
+    Generate date ranges for a seasonal time series
+
+    Args:
+        params (dict): Arguments for the pandas date_range function
+
+    Returns:
+
+    """
+    return pd.date_range(**params)
+
+
+def seasons(df, start_mon, start_d, end_mon, end_d, periods=None, freq='D', **kwargs):
+    """
+
+    Args:
+        df:
+        start_mon:
+        start_d:
+        end_mon:
+        end_d:
+        periods:
+        freq:
+        **kwargs:
+
+    Returns:
+
+    """
+    return OrderedDict([(y,
+                         date_range({'start': dt.datetime(y, start_mon, start_d),
+                                     'end': dt.datetime(y, end_mon, end_d),
+                                     'periods': periods,
+                                     'freq': freq}))
+
+                        for y in years(df)])
+
+
+def stats(arr):
+    """
+    Return the statistics for an input array of values
+
+    Args:
+        arr (np.ndarray)
+
+    Returns:
+        OrderedDict
+
+    """
+    try:
+        return OrderedDict([('min', arr.mean()),
+                            ('max', arr.max()),
+                            ('mean', arr.mean()),
+                            ('std', arr.std())])
+
+    except ValueError:  # Can happen if the input array is empty
+        return OrderedDict([('min', None),
+                            ('max', None),
+                            ('mean', None),
+                            ('std', None)])
+
+
+def get_seasonal_info(df, params):
+    """
+    A wrapper function for easily returning the statistics on a seasonal basis for a given field of the data frame
+
+    Args:
+        df (pd.DataFrame)
+        params (dict)
+
+    Returns:
+        OrderedDict
+
+    """
+
+    __seasons = seasons(df, **params)
+
+    return OrderedDict([
+        (y, stats(
+            values(
+                mask(
+                    dates(df, (__seasons[y][0], __seasons[y][-1])), **params
+                ), **params
+            )
+        )
+         )
+        for y in years(df)
+    ])
+
+
+def values(df, field, **kwargs):
+    """
+    Return values from a specific field of the data frame within a given time extent
+
+    Args:
+        df (pd.DataFrame): The exported TAP tool data
+        field (str): The field representing the column name
+
+    Returns:
+        np.ndarray: An array of the time-specified values
+
+    """
+    return df[field].values
+
+
+def plot_data(d, field):
+    """
+    Return the x and y series to be used for plotting
+
+    Args:
+        d (OrderedDict)
+        field (str)
+
+    Returns:
+        Tuple[list, list]:
+            [0] The x-series
+            [1] The y-series
+
+    """
+    return ([year for year in d.keys() if d[year][field] is not None],
+            [i[field] for k, i in d.items() if i[field] is not None])
+
+
+def mask(df, mask_values=mask_values, mask_field='qa', **kwargs):
     """
     Remove rows from the data frame that match a condition
 
     Args:
         df (pd.DataFrame): The input data
-        value (Number[int, float]): The value used to filter the data frame, rows == value will be removed!
-        field (str): The field to use for filtering
+        mask_value (Number[int, float]): The value used to filter the data frame, rows == value will be removed!
+        mask_field (str): The field to use for filtering
 
     Returns:
         pd.DataFrame
 
     """
-    return df[df[field] != value].reset_index(drop=True)
+    return df[~df[mask_field].isin(np.array(mask_values))].reset_index(drop=True)
