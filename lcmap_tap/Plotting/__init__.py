0c9874ef0fa1d20640d3c7462d5fa510eb82b9db
Zelenak <dzelenak@contractor.usgs.gov>
2018-12-11 15:21:34 -0600

    Added variable to hold legend marker settings

--- lcmap_tap/Plotting/__init__.py
+++ lcmap_tap/Plotting/__init__.py
@@ -19,27 +19,40 @@ NAMES = {1: 'Developed',
          8: 'Barren',
          9: 'No Model-fit'}
 
-DEFAULTS = {'highlight_pick': {'marker': 'D', 'ms': 12, 'c': 'none', 'mec': 'lime',  # plot params
+LEG_DEFAULTS = {'highlight_pick': {'marker': 'D', 'ms': 8, 'color': 'none', 'mec': 'lime',  # plot params
                                'mew': 1.75, 'linewidth': 0, 'label': 'Selected', 'picker': 3},
 
-            'highlight_pick_leg': {'marker': 'D', 'ms': 8, 'c': 'none', 'mec': 'lime',  # plot params
+                'clear_obs': {'marker': 'o', 'ms': 8, 'color': "green", 'mec': 'k', 'mew': 0.3, 'linewidth': 0,
+                                  'label': 'Clear'},
+
+                'mask_obs': {'marker': 'o', 'ms': 4, 'color': '0.65', 'label': 'Masked', 'linewidth': 0},
+
+                'out_obs': {'marker': 'o', 'ms': 4, 'color': 'red', 'mec': 'k', 'mew': 0.3,
+                                            'label': 'Unused', 'linewidth': 0},
+
+                'end_lines': {'color': 'maroon', 'linewidth': 1.5, 'label': 'End Date'},
+
+                'break_lines': {'color': 'r', 'linewidth': 1.5, 'label': 'Break Date'},
+
+                'start_lines': {'color': 'b', 'linewidth': 1.5, 'label': 'Start Date'},
+
+                'match_lines': {'color': 'magenta', 'linewidth': 1.5, 'label': 'Start Date = Break Date'},
+
+                'model_lines': {'color': 'orange', 'linewidth': 3, 'alpha': 0.8, 'label': 'Model Fit'},
+
+                'date_lines': {'color': 'dimgray', 'linewidth': 1.5, 'label': 'Datelines'}
+                }
+
+DEFAULTS = {'highlight_pick': {'marker': 'D', 'ms': 12, 'color': 'none', 'mec': 'lime',  # plot params
                                'mew': 1.75, 'linewidth': 0, 'label': 'Selected', 'picker': 3},
 
-            'clear_obs': {'marker': 'o', 's': 44, 'c': 'green', 'edgecolors': 'black',  # scatter params
+            'clear_obs': {'marker': 'o', 's': 44, 'color': 'green', 'edgecolors': 'black',  # scatter params
                           'label': 'Clear', 'picker': 3},
-
-            'clear_obs_leg': {'marker': "o", 'ms': 8, 'color': "green", 'mec': 'k', 'mew': 0.3, 'linewidth': 0,
-                                'label': 'Clear'},
 
             'mask_obs': {'marker': 'o', 's': 21, 'color': '0.65', 'label': 'Masked', 'picker': 2},  # scatter params
 
-            'mask_obs_leg': {'marker': 'o', 'ms': 4, 'color': '0.65', 'label': 'Masked', 'linewidth': 0},
-
             'out_obs': {'marker': 'o', 's': 21, 'color': 'red', 'edgecolors': 'black',  # scatter params
                         'label': 'Unused', 'picker': 3},
-
-            'out_obs_leg': {'marker': 'o', 'ms': 4, 'color': 'red', 'mec': 'k', 'mew': 0.3,
-                            'label': 'Unused', 'linewidth': 0},
 
             'end_lines': {'color': 'maroon', 'linewidth': 1.5, 'label': 'End Date'},
 
@@ -54,7 +67,8 @@ DEFAULTS = {'highlight_pick': {'marker': 'D', 'ms': 12, 'c': 'none', 'mec': 'lim
             'date_lines': {'color': 'dimgray', 'linewidth': 1.5, 'label': 'Datelines'}
             }
 
-LOOKUP = {"Selected": 'highlight_pick', "Clear": 'clear_obs', "Masked": 'mask_obs', "Unused": 'out_obs',
+LOOKUP = {"Selected": 'highlight_pick', "Clear": 'clear_obs',
+          "Masked": 'mask_obs', "Unused": 'out_obs',
           "End Date": 'end_lines', "Break Date": 'break_lines', "Start Date": 'start_lines',
           "Start Date = Break Date": 'match_lines', "Model Fit": 'model_lines', "Datelines": 'date_lines'}
 
