fb01f22d77ba748c2039e1f5a8bed0450fa1a01e
Zelenak <dzelenak@contractor.usgs.gov>
2018-12-11 15:18:34 -0600

    Fixed application of new settings to legend

--- lcmap_tap/Plotting/plot_config.py
+++ lcmap_tap/Plotting/plot_config.py
@@ -1,7 +1,8 @@
 """These are artists on the plot figure that we want to set symbology for"""
 
 from lcmap_tap import HOME
-from lcmap_tap.Plotting import DEFAULTS, LOOKUP
+from lcmap_tap.logger import log
+from lcmap_tap.Plotting import DEFAULTS, LOOKUP, LEG_DEFAULTS
 
 import os
 import yaml
@@ -21,18 +22,34 @@ class PlotConfig:
                 self.opts = yaml.load(yaml_file)
 
         else:
-            self.opts = DEFAULTS
+            self.opts = {'DEFAULTS': DEFAULTS, 'LEG_DEFAULTS': LEG_DEFAULTS}
 
     def update_config(self, item, params):
+        log.debug('PARAMS: {}'.format(params))
+
         _id = LOOKUP[item]
 
-        temp = self.opts[_id]
+        if _id is 'highlight_pick':
+            # 's' won't work for plots, only for scatters
+            params['ms'] = params['s']
 
-        for key in temp.keys():
-            if key in params.keys():
-                temp[key] = params[key]
+            params.pop('s', None)
 
-        self.opts[item] = temp
+        temp = self.opts['DEFAULTS'][_id]
+
+        for key, value in params.items():
+            temp[key] = value
+
+        self.opts['DEFAULTS'][_id] = temp
+
+        temp = self.opts['LEG_DEFAULTS'][_id]
+
+        for key, value in params.items():
+            # Don't want to change symbol size on the legend
+            if key is not 's':
+                temp[key] = value
+
+        self.opts['LEG_DEFAULTS'][_id] = temp
 
     def save_config(self):
         with open(config_file, 'w') as f:
