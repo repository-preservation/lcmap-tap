b28f46a320431edd837c39a6af4b2761d95dfae3
danzelenak-usgs <dzelenak@contractor.usgs.gov>
2019-10-16 13:34:26 -0500

    change default directory to 90daytemp

--- lcmap_tap/__init__.py
+++ lcmap_tap/__init__.py
@@ -1,11 +1,10 @@
 
 import os
-from pathlib import Path
 
 # Get the path to the home directory for the current user and create an 'lcmap_tap' subfolder
-HOME = os.path.join(str(Path.home()), 'lcmap_tap')
+HOME = os.path.join('K:\90daytemp', r'TAP_Tool_files')
 
 if not os.path.exists(HOME):
     os.makedirs(HOME)
 
-__version__ = '1.1.0'
+__version__ = '1.2.0'
